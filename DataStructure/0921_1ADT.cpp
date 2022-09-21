8ae0d2fb267f2339bad19ebfb54312e8dc0c038d
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-09-21 10:15:55 +0900

    dsADT

---
+++ DataStructure/0921_1ADT.cpp
@@ -0,0 +1,19 @@
+#include < stdio.h>
+
+typedef struct _wallet	//동전 및 지폐 일부만을 대상으로 표현한 지갑
+{
+	int coin100Num;		//100원짜리 동전의 수
+	int bill500Num;		//5,000원짜리 지폐의 수
+
+	int TakeOutMoney(Wallet* pw, int coinNum, int billNum);
+	void PutMoney(Wallet* pw, int coinNum, int billNum);
+} Wallet;
+
+int main(void)
+{
+	Wallet myWallet; //지갑 마련
+
+	PutMoney(&myWallet, 5, 10); //돈을 넣는다.  
+
+	ret = TakeOutMoney(&myWallet, 2, 5); //돈을 꺼낸다.
+}
